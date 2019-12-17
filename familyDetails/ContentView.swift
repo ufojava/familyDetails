9030838fbbcf33971b8cba6832cf77c6b31802b9
Ufuoma Okoro <ufookoro@gmail.com>
2019-12-17 18:52:48 +0000

    Initial Commit

---
+++ familyDetails/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  familyDetails
+//
+//  Created by Ufuoma Okoro on 17/12/2019.
+//  Copyright © 2019 Ufuoma Okoro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
